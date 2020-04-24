e12ede91cb8057e4b00f2e2783a3ca6a38d52288
Jason Merino <jason.merino@ferguson.com>
2020-04-23 17:03:25 -0700

    Adds navigation container and navigation stack.

--- App.tsx
+++ App.tsx
@@ -1,19 +1,23 @@
+import 'react-native-gesture-handler';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { Home } from './screens/home.screen';
+
+const AppStack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <NavigationContainer>
+      <AppStack.Navigator>
+        <AppStack.Screen
+          options={{
+            header: () => null,
+          }}
+          name="Home"
+          component={Home}
+        />
+      </AppStack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
